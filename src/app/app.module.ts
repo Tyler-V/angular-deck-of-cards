e55fd21381d13c5d140936c105c3932d9a1a8cda
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2018-10-25 20:54:05 -0400

    Update build path, add firebase deploy for demo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { DeckOfCardsModule } from './features/deck-of-cards/deck-of-cards.module';
+import { MaterialModule } from './shared/modules/material.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     DeckOfCardsModule,
   ],
   declarations: [
