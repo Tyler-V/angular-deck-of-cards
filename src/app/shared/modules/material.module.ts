d1043840e9b2c5b97a9f0bbc9216035e85b6f338
Soma Froghyar <froghyar.soma@gmail.com>
2020-03-24 18:12:37 +0000

    ng material update

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,10 +1,13 @@
 
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {
-  MatButtonModule, MatCheckboxModule, MatSelectModule,
-  MatSliderModule, MatCardModule, MatToolbarModule, MatInputModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [FlexLayoutModule, MatButtonModule, MatCheckboxModule,
