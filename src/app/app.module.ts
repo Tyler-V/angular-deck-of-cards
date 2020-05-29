11b7183dd04e7de063008deff2e0009b084629b2
Soma Froghyar <froghyar.soma@gmail.com>
2020-05-29 16:21:16 +0100

    updating endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { RouterModule } from '@angular/router';
 import { SozGottaWaitComponent } from './features/soz-gotta-wait/soz-gotta-wait.component';
 import { UserStripComponent } from './rooms/lobby-room/user-strip/user-strip.component';
 
-const config: SocketIoConfig = { url: '64.227.32.214:4444', options: {} };
+const config: SocketIoConfig = { url: 'http://64.227.32.214:4444', options: {} };
 
 @NgModule({
   imports: [
